b8fd66802e97f3a43ba7471df8fd56a9e06effcb
Mrityunjayyshukla <shuklamrityunjay60@gmail.com>
2026-09-16 23:14:57 +0530

    all dataset cleaning functions

--- batch_cleaning.py
+++ batch_cleaning.py
@@ -49,6 +49,12 @@ def create_mapping(df, column_name, map_dict):
     return df
 
 # Change datatypes
+def change_dtypes(df, columns, dtypes):
+    if not isinstance(dtypes, list):
+        dtypes = [dtypes] * len(columns)
+    for col, dtype in zip(columns, dtypes):
+        df[col] = df[col].astype(dtype)
+    return df
 
 # Creating timestamp Format
 def timestamp_format(df, column_list):
@@ -68,4 +74,115 @@ def timestamp_format(df, column_list):
 
     return df
 
-# Timestamp format if NaT values are also there
+# Get unique values in a column
+def unique_values(df, column):
+    print(df[column].unique())
+
+
+# Lap Data Functions
+# Remove Formation Laps
+def remove_formation_laps(df):
+    df = df[df['LapNumber'] != 0]
+    return df
+
+# Handling in-laps and out-laps
+def in_out_laps(df):
+    df['IsInLap'] = df['PitInTime'].isna()
+    df['IsOutLap'] = df['PitOutTime'].isna()
+    return df
+
+# Track Status Handling
+trackStatus_map = {
+    "1": "Track Clear", "2": "Yellow Flag",
+    "4": "Safety Car deployed", "5": "Red Flag (Session Suspended)",
+    "6": "Virtual Safety Car deployed","7": "Virtual Safety Car ending"
+}
+def track_status(df):
+    status_list = []
+    df['TrackStatus'].to_string()
+    for i in df['TrackStatus']:
+        status = ""
+        for j in i:
+            status += trackStatus_map[j] + "-"
+        status_list.append(status)
+    df['TrackData'] = status_list
+    df['TrackData'] = df['TrackData'].str.rstrip("-")
+    return df
+
+
+# Telemetry Data Functions
+# Removing impossible values
+def tele_impossible_values(df):
+    # Speed
+    df['Speed'] = df[(df['Speed'] >= 0) | (df['Speed'] < 400)]
+    # Throttle
+    df['Throttle'] = df[(df['Throttle'] >= 0) | (df['Throttle'] <= 100)]
+    # Braking
+    df['Brake'] = df[(df['Brake'].isna()) | (df['Brake'].isin([True, False]))]
+    return df
+
+# Synchronizing Telemetry Frequency
+def synchronize_frequency(df):
+    df['Date'] = pd.to_datetime(df['Date'])
+    df = df.set_index('Date')
+    df = df[~df.index.duplicated(keep='first')]
+
+    # Seperate columns by type to apply interpolation rules
+    # - Continous (Speed, throttle, RPM) need linear interpolation
+    # - Discrete (nGear, DRS, Brake) need forward fill
+    continuous_cols = ['Speed', 'RPM', 'Throttle', 'X', 'Y', 'Z']
+    discrete_cols = ['nGear', 'DRS', 'Brake']
+    existing_cont = [col for col in continuous_cols if col in df.columns]
+    existing_disc = [col for col in discrete_cols if col in df.columns]
+    rule = '100ms'
+    df = pd.concat([
+        df[existing_cont].resample(rule).mean().interpolate(method='linear'),
+        df[existing_disc].resample(rule).ffill()
+    ], axis=1)
+
+    df = df.reset_index()
+    return df
+
+# Remove Corrupted GPS points
+def corrupted_GPS(df):
+    df = df.dropna(subset=['X','Y'])
+    df = df[~((df['X']==0) & (df['Y']==0))]
+
+    # Filter out spatial jumps
+    df['DeltaX'] = df['X'].diff()
+    df['DeltaY'] = df['Y'].diff()
+    df["DistanceStep"] = np.sqrt(df['DeltaX']**2 + df['DeltaY']**2)
+    if 'Date' in df.columns:
+        parsed_dates = pd.to_datetime(df['Date'], errors='coerce')
+        df['TimeStep'] = parsed_dates.diff().dt.total_seconds()
+        df['TimeStep'] = df['TimeStep'].fillna(0.1)
+    else:
+        df['TimeStep'] = 0.1
+
+    max_plausible_speed = 400
+    df['ImpliedSpeed'] = np.where(df['TimeStep'] > 0, df['DistanceStep']/df['TimeStep'], 0)
+    df = df[df['ImpliedSpeed'] <= max_plausible_speed]
+    df = df.drop(columns=['DeltaX', 'DeltaY', 'DistanceStep','TimeStep','ImpliedSpeed'])
+    return df
+
+# Weather Data Functions
+# Handling impossible values
+def weather_impossible_values(df):
+    df = df[(df['AirTemp'] >= 10) | (df['AirTemp'] <= 45)]
+    df = df[(df['TrackTemp'] >= 15) | (df['TrackTemp'] <= 60)]
+    df = df[(df['Humidity'] >= 10) | (df['Humidity'] <= 100)]
+    df = df[(df['WindSpeed'] >= 0) | (df['WindSpeed'] <= 15)]
+    df = df[(df['WindDirection'] >= 0) | (df['WindDirection'] <= 360)]
+    return df
+
+# DNF, DNS and DSQ flag
+def retire_flag(df, column_name="RaceOutcome"):
+    dnfs = ['Retired', 'Accident', 'Collision', 'Engine', 'Gearbox', 'Power Unit', 'Suspension', 'Brakes', 'Overheating']
+    conditions = [
+        df['Status'].isin(dnfs),
+        df['Status'] == 'DNS',
+        df['Status'] == 'Disqualified'
+    ]
+    choices = ['DNF', 'DNS', 'DSQ']
+    df[column_name] = np.select(conditions, choices, default='Finished')
+    return df
